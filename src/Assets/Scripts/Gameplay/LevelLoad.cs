b6c9fa02b33bb9ea52e7098512529e0a8c462eb0
Chad Smith <chadsmith4@my.unt.edu>
2019-02-13 23:43:35 -0600

    Level loading into start screen from splash screen

---
+++ src/Assets/Scripts/Gameplay/LevelLoad.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Gameplay
+{
+    public class LevelLoad : MonoBehaviour
+    {
+        private int _currentSceneIndex;
+        [SerializeField] private int _timeToWait = 3;
+
+        /// <summary>
+        /// gives you if the current scene is the splash screen.
+        /// </summary>
+        private bool IsSplashScreen
+        {
+            get
+            {
+                return _currentSceneIndex == 0;
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            if(IsSplashScreen)
+            {
+                StartCoroutine(WaitForSplash());
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// Loads the very next scene by moving to the next scene in the build settings.
+        /// </summary>
+        public void LoadNextScene()
+        {
+            SceneManager.LoadScene(_currentSceneIndex + 1);
+        }
+
+        private IEnumerator WaitForSplash()
+        {
+            yield return new WaitForSeconds(_timeToWait);
+
+            LoadNextScene();
+        }
+    }
+}
+
