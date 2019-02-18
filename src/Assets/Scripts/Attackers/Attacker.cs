f0ef32fdaf5a47665e4c3e90eccc315ef4e94fb8
Chad Smith <chadsmith4@my.unt.edu>
2019-02-17 23:45:49 -0600

    basic movement for attacker

---
+++ src/Assets/Scripts/Attackers/Attacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Attacker
+{
+    public class Attacker : MonoBehaviour
+    {
+        [Range(0f, 5f)]
+        [SerializeField]
+        private float _walkSpeed = 1f;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var movementSpeed = Vector2.left * _walkSpeed * Time.deltaTime;
+            transform.Translate(movementSpeed);
+        }
+    }
+}
