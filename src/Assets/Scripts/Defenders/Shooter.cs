4e1b6b9b5239ae1186d3776b840c081d64ad5862
Chad Smith <chadsmith4@my.unt.edu>
2019-02-18 22:06:32 -0600

    Projectile spawn for attackers at attack animation event

---
+++ src/Assets/Scripts/Defenders/Shooter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Attacker
+{
+    public class Shooter : MonoBehaviour
+    {
+        [SerializeField] private GameObject _projectile;
+        [SerializeField] private GameObject _gun;
+
+        public void Fire()
+        {
+            Instantiate(_projectile, _gun.transform.position, _gun.transform.rotation);
+        }
+    }
+}
