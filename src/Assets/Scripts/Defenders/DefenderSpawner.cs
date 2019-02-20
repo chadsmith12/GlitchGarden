a689db0066134ad3074472f0dea5bcf084301a0d
Chad Smith <chadsmith4@my.unt.edu>
2019-02-19 23:36:25 -0600

    spawn defenders on mouse click

---
+++ src/Assets/Scripts/Defenders/DefenderSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Defefenders
+{
+    public class DefenderSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _defender;
+
+        private void OnMouseDown()
+        {
+            SpawnDefender();
+        }
+
+        private void SpawnDefender()
+        {
+            var defender = Instantiate(_defender, transform.position, Quaternion.identity);
+        }
+    }
+}
