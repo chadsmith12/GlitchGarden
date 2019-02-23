5fdc0e364c72e46b376eb7206dbba85ae6e10023
Chad Smith <chadsmith4@my.unt.edu>
2019-02-22 22:59:57 -0600

    adding resources on animation event for trophy

--- src/Assets/Scripts/Defenders/Defender.cs
+++ src/Assets/Scripts/Defenders/Defender.cs
@@ -1,23 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Assets.Scripts.Gameplay;
 
 namespace Assets.Scripts.Defefenders
 {
     public class Defender : MonoBehaviour
     {
+        private StarDisplay _starDisplay;
         [SerializeField] private int _starCost = 100;
 
-        // Use this for initialization
-        void Start()
+        private void Start()
         {
-
+            _starDisplay = FindObjectOfType<StarDisplay>();
         }
 
-        // Update is called once per frame
-        void Update()
+        /// <summary>
+        /// Adds the number of stars this defender generated to the star display.
+        /// </summary>
+        /// <param name="amount">The number of stars that were generated.</param>
+        public void AddStars(int amount)
         {
-
+            _starDisplay.AddStars(amount);
         }
     }
 }
